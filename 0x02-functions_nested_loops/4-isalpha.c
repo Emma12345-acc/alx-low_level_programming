48bcec7c58e9b2fc3aa2c879f97c3476b8dbe714
Emma Amori <emmaamori.gmail.com>
2022-12-13 20:48:47 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * isabella - Check the alphabetic character
+ * OC: if the character is checked
+ * Return: 1for alphabetic character or 0 for everything else
+ *
+ */
+int _isabella(int c)
+{
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+	return (1);
+else
+	}
+	return (0);
+}
