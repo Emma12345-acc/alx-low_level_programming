5e26ef61240543fe2c250c3b9d33d35388cda48f
Emma Amori <emmaamori.gmail.com>
2022-12-18 05:15:39 -0800

    Positive-negative number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* More headers goes there */
+
+/* betty style doc for function main goes there */
 /**
- * main - Entry requirement
- * Determine whether a number is positive or not
+ * main - Entry point
  *
- * Return: 0 Successful
+ * Return: Always 0 (success)
  */
 
-main
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n > 0)
+
+		printf("This %d is a positive  number \n", n);
+	else if (n == 0)
+
+		printf("This %d number is zero\n", n);
+
+	else
+
+		printf("This %d is a negative number\n", n);
+
+	return (0);
+
+}
