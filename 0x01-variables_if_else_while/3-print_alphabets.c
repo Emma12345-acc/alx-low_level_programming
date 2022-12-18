954e032c15e77b8a33c599f6abb83355ddad1e52
Emma Amori <emmaamori.gmail.com>
2022-12-18 12:52:33 -0800

    alphabet from small to capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+
+	for (c = 'a'; c <= 'm'; c++)
+	{putchar(c);
+	}
+	for (d = 'N'; d <= 'Z'; d++)
+	{putchar(d);
+	}
+	putchar('\n');
+		return (0);
+}
+
+
