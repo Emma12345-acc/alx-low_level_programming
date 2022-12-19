a898df7e1399dbe6141eac17ddb49b9899f3e74e
Emma Amori <emmaamori.gmail.com>
2022-12-18 21:30:08 -0800

    Print single numbers of base 10

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char c;
 
-	while (c <= 'z')
-	{
-		if ((c != 'q' && c != 'e') && c <= 'z')
-			putchar(c);
-		c++;
-	}
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all single number of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
