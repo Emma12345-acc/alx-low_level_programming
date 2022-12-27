b81858de78a5f2eb9d877a6fdca1db016198dcbb
Emma Amori <emmaamori.gmail.com>
2022-12-27 13:13:52 -0800

    fixed issues

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - print single digit numbers
  *
- * Return: Always 0 Success
- *
+ * Description: print single digits numbers with commas
+ * Return: always return 0
  */
-
 int main(void)
 {
-
 	int i;
-
 	int j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 10; i <= 19; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 10; j <= 19; j++)
 		{
-			if (i > j)
-				continue;
-			putchar(i + '0');
-			putchar(j + '1');
-			if (i <= 7)
-				putchar(',');
-			putchar(' ');
+			if ((j % 10) > (i % 10))
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
-
