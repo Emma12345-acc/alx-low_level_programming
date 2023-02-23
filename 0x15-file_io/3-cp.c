b0be892e87dac8cc631a19fc5d1e1eef4a396545
Emma12345-acc <edesiri_amori@yahoo.com>
2023-02-23 09:49:23 -0800

    A program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,7 +39,6 @@ exit(100);
 /**
 * main - Copies the contents of a file to another file.
 * @argc: The number of arguments supplied to the program.
-
 * @argv: An array of pointers to the arguments.
 *
 * Return: 0 on success.
