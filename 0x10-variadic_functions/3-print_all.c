b74ad4ac4b3c3be03db50728f2666f95e019e0ae
Emma12345-acc <edesiri_amori@yahoo.com>
2023-02-23 06:08:29 -0800

    A function that prints strings, followed by a new line

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,14 +1,17 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
 * print_all - prints anything
 * @format: list of types of arguments passed to the function
 */
+
 void print_all(const char * const format, ...)
 {
 int i = 0;
 char *str, *sep = "";
+
 va_list list;
 va_start(list, format);
 if (format)
@@ -35,6 +38,7 @@ break;
 default:
 i++;
 continue;
+
 }
 sep = ", ";
 i++;
